bf9c012331a73caaf8bd1686154432bcf3092a42
Eric Majidian <emajidian@mail.csuchico.edu>
2018-06-07 14:43:56 -0600

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# decode.py
+# Implementation of encoding and decoding algorithms as described in the paper
+# "A new algorithm for encoding and decoding the Hilbert order" by Ningtao
+# Chen, Nengchao Wang, and Baochang Shi
+#
+# Chen, N., Wang, N., & Shi, B. (2007). Efficient algorithm for encoding and
+# decoding the Hilbert order. Software-Practice and Experience, 37(8), 897-908.
+
+import math
+import sys
+
+if not len(sys.argv) == 3:
+    print("Usage: provide z and n as command line arguments.")
+    quit()
+
+# ----- INITIALIZATION -----
+z = str(sys.argv[1])
+n = int(sys.argv[2])
+
+r = z[-1:]
+z = z[:-1]
+w = 2
+
+if r == "0":
+    x = 0; y = 0
+elif r == "1":
+    x = 0; y = 1
+elif r == "2":
+    x = 1; y = 1
+else:
+    x = 1; y = 0
+
+
+# ----- ITERATION -----
+while len(z) > 0:
+    r = z[-1:]
+    z = z[:-1]
+    if r == "0":
+        tmp = x
+        x = y
+        y = tmp
+    elif r == "1":
+        y = y + w
+    elif r == "2":
+        x = x + w
+        y = y + w
+    else:
+        tmp = x
+        x = (w*2)-y-1
+        y = w-tmp-1
+    w = w*2
+
+
+if (x==0) and (y==0):
+    rmin = 1
+else:
+    rmin = int(math.floor(math.log(int(max(x,y)), 2)) + 1)
+
+if not rmin%2 == n%2:
+    tmp = x
+    x = y
+    y = tmp
+
+print ("(",x,",",y,")")
---
+++ encode.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# encode.py
+# Implementation of encoding and decoding algorithms as described in the paper
+# "A new algorithm for encoding and decoding the Hilbert order" by Ningtao
+# Chen, Nengchao Wang, and Baochang Shi
+#
+# Chen, N., Wang, N., & Shi, B. (2007). A new algorithm for encoding and
+# decoding the Hilbert order. Software-Practice and Experience, 37(8), 897-908.
+
+
+import math
+import sys
+
+def getQuadrant(x, y, rmin):
+    mwidth = 2**(rmin)   # matrix width
+    qwidth = 2**(rmin-1) # quadrant width
+    if ((0 <= x) and (x < qwidth)) and ((0 <= y) and (y < qwidth)):
+        return 0
+    elif ((0 <= x) and (x < qwidth)) and ((qwidth <= y) and (y < mwidth)):
+        return 1
+    elif ((qwidth <= x) and (x < mwidth)) and ((qwidth <= y) and (y < mwidth)):
+        return 2
+    else:
+        return 3
+
+if not len(sys.argv) == 4:
+    print("Usage: provide x, y, and n as command line arguments.")
+    quit()
+
+# ----- INITIALIZATION -----
+x = int(sys.argv[1])
+y = int(sys.argv[2])
+n = int(sys.argv[3])
+
+z = ""
+
+if (x==0) and (y==0):
+    rmin = 1
+else:
+    rmin = int(math.floor(math.log(int(max(x,y)), 2)) + 1)
+
+w = 2**(rmin-1)
+
+
+# if parities of n and rmin do not match, swap x and y
+if not rmin%2 == n%2:    
+    tmp = x
+    x = y
+    y = tmp
+
+
+# ----- ITERATION -----
+while not rmin == 0:
+    Q = getQuadrant(x, y, rmin)
+    z += str(Q)
+    if (Q == 0):
+        tmp = x
+        x = y
+        y = tmp
+    elif (Q == 1):
+        y = y - w
+    elif (Q == 2):
+        x = x - w
+        y = y - w
+    else:
+        tmp = x
+        x = w - y - 1 
+        y = (w*2) - tmp - 1
+
+    rmin = rmin - 1
+    w = w / 2
+
+print (z)
